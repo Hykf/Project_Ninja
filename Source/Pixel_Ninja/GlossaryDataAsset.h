9b9924894edb829450c24cd03408e5ed64d5119a
s24729-pj <s24729@pjwstk.edu.pl>
2023-12-02 17:54:14 +0100

    Added basic glossary and fixed overlapping widgets

---
+++ Source/Pixel_Ninja/GlossaryDataAsset.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSprite.h"
+#include "Engine/DataAsset.h"
+#include "GlossaryDataAsset.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGlossaryEntry
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPaperSprite* Sprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+};
+
+UCLASS(BlueprintType)
+class PIXEL_NINJA_API UGlossaryDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+	public:
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FGlossaryEntry> GlossaryEntries;
+};
